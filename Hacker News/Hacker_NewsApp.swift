6c1858470e7aca845d70dd6f5580debf630f3846
Utku Kaan Gülsoy <47040490+utkugulsoy@users.noreply.github.com>
2022-11-09 01:44:36 +0300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Utku Kaan Gülsoy on 9.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
